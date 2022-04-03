1b01489d3cf25d88f7c03da537479d070d551fc7
Zlatko Tolić <quexpexx@pm.me>
2022-04-03 07:56:38 +0200

    added userscript

---
+++ userscript.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         r/place overlay
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the canvas!
+// @author       oralekin - script, Quexpexx#6969 - image
+// @match        https://hot-potato.reddit.com/embed*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
+// @grant        none
+// ==/UserScript==
+if (window.top !== window.self) {
+    window.addEventListener('load', () => {
+            document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
+        (function () {
+            const i = document.createElement("img");
+            i.src = "https://raw.githubusercontent.com/quexpexx/r-place-overlay/main/template.png";
+            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
+            console.log(i);
+            return i;
+        })())
+
+    }, false);
+
+}
