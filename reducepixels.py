8de6048e6b96d0e1f90f6adafd7054b74912c710
Cale111 <34945905+Cale111@users.noreply.github.com>
2022-04-03 03:04:01 -0700

    added consistency and comments

--- reducepixels.py
+++ reducepixels.py
@@ -1,14 +1,17 @@
 from PIL import Image
 
-canvas_file = Image.open('template.png')
-canvas_expanded = canvas_file.load()
-contracted_size = (canvas_file.size[0]//3, canvas_file.size[1]//3)
-canvas = Image.new('RGBA', contracted_size, (0, 0, 0, 0))
+# Open large template file and calculate reduced size
+canvas_expanded = Image.open('template.png')
+reduced_size = (canvas_expanded.size[0]//3, canvas_expanded.size[1]//3)
+# Create reduced image, blank
+canvas = Image.new('RGBA', reduced_size, (0, 0, 0, 0))
 
-for x in range(1, canvas_file.size[0], 3):
-    for y in range(1, canvas_file.size[1], 3):
-        color = canvas_expanded[x, y]
+# Loop through every 3rd pixel starting on the 2nd of each axis
+for x in range(1, canvas_expanded.size[0], 3):
+    for y in range(1, canvas_expanded.size[1], 3):
+        color = canvas_expanded.getpixel((x, y))
         new_x, new_y = (x-1)//3, (y-1)//3
+        # Put pixel on reduced image
         canvas.putpixel((new_x, new_y), color)
 
 canvas.save('canvas.png')
