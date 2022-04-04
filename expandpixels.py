aa8556c2b81c79ff59eb4104ede41e9b181e4eda
Cale111 <34945905+Cale111@users.noreply.github.com>
2022-04-03 17:19:18 -0700

    extremely hacky and slightly broken palette version of scripts
    if you can find a way to make this better, please do.

--- expandpixels.py
+++ expandpixels.py
@@ -1,10 +1,14 @@
-from PIL import Image
+from PIL import Image, ImagePalette
 
 # Open canvas file and calculate expanded size
 canvas = Image.open('canvas.png')
 expanded_size = (canvas.size[0]*3, canvas.size[1]*3)
+# Get palette colors
+canvaspalette = canvas.getpalette('RGBA')
+canvaspalette[3] = 0 # Hacky, but I really don't know why it doesn't give 0. Especially when it's transparent in my editor.
 # Create expanded image, blank
-canvas_expanded = Image.new('RGBA', expanded_size, (0, 0, 0, 0))
+canvas_expanded = Image.new('P', expanded_size, 0)
+canvas_expanded.putpalette(canvaspalette, 'RGBA')
 
 # Loop through every pixel
 print("Expanding pixels...")
@@ -16,5 +20,5 @@ for x in range(canvas.size[0]):
         # Put pixel on reduced image
         canvas_expanded.putpixel((new_x, new_y), color)
 
-canvas_expanded.save('template.png')
+canvas_expanded.save('template.png') # For some reason, when it saves, it adds an extra 11 black colors to the palette. It doesn't appear in the palette until saving.
 print("Complete!")
--- reducepixels.py
+++ reducepixels.py
@@ -3,8 +3,12 @@ from PIL import Image
 # Open large template file and calculate reduced size
 canvas_expanded = Image.open('template.png')
 reduced_size = (canvas_expanded.size[0]//3, canvas_expanded.size[1]//3)
+# Get palette colors
+canvaspalette = canvas_expanded.getpalette('RGBA')
+canvaspalette[3] = 0 # Hacky, but I really don't know why it doesn't give 0. Especially when it's transparent in my editor.
 # Create reduced image, blank
-canvas = Image.new('RGBA', reduced_size, (0, 0, 0, 0))
+canvas = Image.new('P', reduced_size, 0)
+canvas.putpalette(canvaspalette, 'RGBA')
 
 # Loop through every 3rd pixel starting on the 2nd of each axis
 print("Reducing pixels...")
@@ -15,5 +19,5 @@ for x in range(1, canvas_expanded.size[0], 3):
         # Put pixel on reduced image
         canvas.putpixel((new_x, new_y), color)
 
-canvas.save('canvas.png')
+canvas.save('canvas.png') # For some reason, when it saves, it adds an extra 11 black colors to the palette. It doesn't appear in the palette until saving.
 print("Complete!")
